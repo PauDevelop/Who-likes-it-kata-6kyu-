1a89737ed1da67a0eebb0bdd6f5b7fc0333c6863
Pablo A <109696953+PauDevelop@users.noreply.github.com>
2023-01-29 10:39:26 +0100

    Multiples of 3 or 5 - 6kyu

---
+++ multiples-of-3-or-5.js
@@ -0,0 +1,13 @@
+function solution(number){
+    let result = 0
+    for (let i = 0; i < number; i++) {
+      if (((i % 3) === 0) && ((i % 5) === 0))  {
+        result += i
+      } else if ((i % 5) === 0) {
+        result += i
+      } else if ((i % 3) === 0) {
+        result += i
+      }
+    }
+    return result
+  }
